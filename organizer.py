8d8f14b66d4f6cdcd4a756d2d20548b15b4486c4
cfan310 <63741693+cfan310@users.noreply.github.com>
2021-03-03 18:07:04 -0800

    Update organizer.py
    -Imported os to manipulate macbook pro files
    -Stored various file extensions as different variables

--- organizer.py
+++ organizer.py
@@ -1,1 +1,12 @@
 # Desktop file organizer
+
+import os                  # importing macbook pro operating system to pycharm to mannipulate its data
+import collections          
+from pprint import pprint   
+
+EXT_AUDIO = ['mp3', 'wav', 'raw', 'wma', 'mid', 'midi']
+EXT_VIDEO = ['mp4', 'mpg', 'mpeg', 'avi', 'mov', 'flv', 'mkv', 'm4a', 'mwv', 'm4v', 'h264']
+EXT_IMGS  = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'bmp', 'psd', 'svg', 'tiff', 'tif']
+EXT_DOCS  = ['txt', 'pdf', 'csv', 'xls', 'xlsx', 'ods', 'doc', 'docx', 'html', 'odt', 'tex', 'ppt', 'pptx', 'log']
+EXT_COMPR = ['zip', 'z', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb']
+EXT_INSTL = ['dmg', 'exe', 'iso']
